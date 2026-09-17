4feca20a5b0591b8f4b8c506fa561d7b96ebc736
VikaKhang286 <khang442000@gmail.com>
2026-09-17 11:36:45 +0700

    update

--- addons/dac_erp/static/src/js/backend/cart_customer_field.js
+++ addons/dac_erp/static/src/js/backend/cart_customer_field.js
@@ -3,8 +3,30 @@
 import { registry } from "@web/core/registry";
 import { Many2OneField, many2OneField } from "@web/views/fields/many2one/many2one_field";
 
+import { useInputField } from "@web/views/fields/input_field_hook";
+
 export class CartCustomerField extends Many2OneField {
     static template = "dac_erp.CartCustomerField";
+
+    setup() {
+        super.setup();
+        useInputField({
+            refName: "customerAddress",
+            fieldName: "customer_address",
+            getValue: () => this.props.record.data.customer_address || "",
+        });
+        useInputField({
+            refName: "customerPhone",
+            fieldName: "phone",
+            getValue: () => this.props.record.data.phone || "",
+        });
+    }
+
+    onAddressKeydown(ev) {
+        if (ev.key === "Enter") {
+            ev.stopPropagation();
+        }
+    }
 }
 
 registry.category("fields").add("dac_cart_customer", {
